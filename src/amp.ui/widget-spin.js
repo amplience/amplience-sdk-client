093c49690e4b68c8b98bbf7f1539b2a4179c5013
Konstantin Solodkyy <kostyantyn.solodkyy@gmail.com>
2015-11-03 14:32:35 +0200

    DSV-274: DFS: Mobile Chrome Android. 360 asset spins and zooms in/out slowly - fixed
    (cherry picked from commit f77da16fefaf6879de3bea703f56470950bff904)

--- src/amp.ui/widget-spin.js
+++ src/amp.ui/widget-spin.js
@@ -68,8 +68,8 @@
             this.toLoadCount =  this.imgs.length;
             this.loadedCount = 0;
             children.addClass('amp-frame');
-            children.css({'z-index':-1});
-            children.eq(this._index-1).css('z-index', 1);
+            children.css({'opacity': 0});
+            children.eq(this._index-1).css('opacity', 1);
             children.eq(this._index-1).addClass(this.options.states.selected + ' ' +this.options.states.seen);
             setTimeout(function(_self) {
                 return function() {
@@ -565,9 +565,9 @@
                 return;
             }
             nextItem.addClass(this.options.states.selected + ' ' +this.options.states.seen);
-            nextItem.css('z-index', 1);
+            nextItem.css('opacity', 1);
             currItem.removeClass(this.options.states.selected);
-            currItem.css('z-index', -1);
+            currItem.css('opacity', 0);
             this._setIndex(_index);
 
             // set the index, but ignore visibility toggling as this is already done
