dd3ec0a22801d0ca81c160883e3c154d9e46e7cb
Arthur Shpakov <arthur@amplience.com>
2016-01-25 13:28:02 +0200

    Moved default timeout param in jsonp

--- src/amp/sdk-get.js
+++ src/amp/sdk-get.js
@@ -143,10 +143,9 @@ function objLength(obj) {
  * @param {Object} assets to load in the format {'name':'asset','type':'i'}
  * @param {Function} success Callback function called on successful load
  * @param {Function} error Callback function called on unsuccessful load
- * @param {Int} integer to change timeout time
  */
-amp.get = function (assets, success, error, videoSort, timeout) {
-    var assCount = 0, failed = true, dataWin = {}, dataFail = {}, assLength = 0, timeout = timeout || 60000;
+amp.get = function (assets, success, error, videoSort) {
+    var assCount = 0, failed = true, dataWin = {}, dataFail = {}, assLength = 0;
 
     var win = function(url){
         return function (name,data) {
@@ -216,14 +215,14 @@ amp.get = function (assets, success, error, videoSort, timeout) {
         if(!isValid(assets))
             return;
         var url = amp.getAssetURL(assets);
-        jsonp(amp.getAssetURL(assets)+ '.js', assets.name, win(url), fail(url),assets.transform, timeout);
+        jsonp(amp.getAssetURL(assets)+ '.js', assets.name, win(url), fail(url),assets.transform);
     }else{
         assLength = assets.length;
         for (var i = 0; i < assLength; i++) {
             if(!isValid(assets[i]))
                 continue;
             var url = amp.getAssetURL(assets[i]);
-            jsonp(url + '.js', assets[i].name, win(url), fail(url),assets.transform, timeout);
+            jsonp(url + '.js', assets[i].name, win(url), fail(url),assets.transform);
         }
     }
 };
@@ -308,7 +307,7 @@ amp.clearJsonCache = function(){
 }
 
 var jsonp =  amp.jsonp = function(url, name, success, error, transform, timeout){
-
+    var timeout = timeout || 60000;
     if(!transform){
         transform = '';
     } else {
